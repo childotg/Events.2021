b213dc328aded631e4ede8c652d324dbfc0d35dd
Roberto Freato <child@ocdstudio.net>
2021-04-22 14:24:53 +0200

    DevOpsCon London release

---
+++ 210422 - DevOpsCon @London/Workloads/DevOpsCon/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace DevOpsCon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = DateTime.UtcNow;
+            while (start.AddMinutes(5) > DateTime.UtcNow)
+            {
+                Console.WriteLine(
+                    "Hello from DevOpsCon 2021 @London");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
