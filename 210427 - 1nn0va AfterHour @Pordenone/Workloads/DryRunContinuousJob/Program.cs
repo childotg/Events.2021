1eed1bd980c12ffbf0df03254462b45e23f2e41c
Roberto Freato <child@ocdstudio.net>
2021-04-27 10:15:41 +0200

    1nn0va release

---
+++ 210427 - 1nn0va AfterHour @Pordenone/Workloads/DryRunContinuousJob/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace DryRunContinuousJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startTime = DateTime.UtcNow;
+            while (startTime.AddMinutes(5) > DateTime.UtcNow)
+            {
+                Console.WriteLine("Running and doing special stuff...");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
