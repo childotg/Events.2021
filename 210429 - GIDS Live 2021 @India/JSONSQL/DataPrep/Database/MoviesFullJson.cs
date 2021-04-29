93e80bc0ebe9daf3f19900d0f623810ce24d2e5d
Roberto Freato <child@ocdstudio.net>
2021-04-29 10:27:54 +0200

    GIDS session release

---
+++ 210429 - GIDS Live 2021 @India/JSONSQL/DataPrep/Database/MoviesFullJson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DataPrep.Database
+{
+    public partial class MoviesFullJson
+    {
+        public string MovieId { get; set; }
+        public string Movie { get; set; }
+        public string Reviews { get; set; }
+    }
+}
---
+++ 210429 - GIDS Live 2021 @India/JSONSQL/Database/Views/playground.vMovies.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [playground].[vMovies] AS (
+SELECT 
+	JSON_VALUE(value,'$.movie_id') AS movie_id,
+	JSON_VALUE(value,'$.plot_summary') AS plot_summary,
+	[json].[fAsTimespan](JSON_VALUE(value,'$.duration')) AS duration,
+	JSON_VALUE(value,'$.genre[0]') AS genre,
+	CAST(JSON_VALUE(value,'$.rating') AS DECIMAL(3,1)) AS rating,
+	TRY_CAST(JSON_VALUE(value,'$.release_date') AS DATE) AS release_date,
+	JSON_VALUE(value,'$.plot_synopsis') AS plot_synopsis
+  FROM [playground].[KeyValue]
+  CROSS APPLY OPENJSON(Doc)
+  WHERE Id=3
+  )
+GO
