5208497ac1deba56906729bfcb58ee5ce29ff00f
Roberto Freato <child@ocdstudio.net>
2021-06-25 15:52:54 +0200

    AzureDay release

---
+++ 210625 - AzureDay 2021 @Rome/dotnetcoreinstall.sh
@@ -0,0 +1,6 @@
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-runtime-5.0
