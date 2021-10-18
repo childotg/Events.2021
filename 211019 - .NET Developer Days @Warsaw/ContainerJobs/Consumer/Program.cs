45ad3d40a271ddb71205d44527074c923ea860ad
childotg <child@ocdstudio.net>
2021-10-18 15:36:25 +0200

    .NET Developer Days S1 draft

---
+++ 211019 - .NET Developer Days @Warsaw/ContainerJobs/Consumer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ConsumerTask().RunAsync().GetAwaiter().GetResult();
+        }
+
+    }
+}
