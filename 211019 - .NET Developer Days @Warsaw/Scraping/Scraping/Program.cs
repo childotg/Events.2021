4a02656f9e62fd17f593692cd53764efcd737ac9
Roberto Freato <child@ocdstudio.net>
2021-10-18 22:39:21 +0200

    NETDD S2 release

---
+++ 211019 - .NET Developer Days @Warsaw/Scraping/Scraping/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scraping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ScrapingTask().RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}
---
+++ 211019 - .NET Developer Days @Warsaw/Scraping/Scraping/ScrapingTask.cs
@@ -0,0 +1,99 @@
+﻿using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scraping
+{
+    internal class ScrapingTask
+    {
+        public ScrapingTask()
+        {
+        }
+
+        public async Task RunAsync()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--disable-background-timer-throttling");
+            options.AddArgument("--disable-backgrounding-occluded-windows");
+            options.AddArgument("--disable-breakpad");
+            options.AddArgument("--disable-component-extensions-with-background-pages");
+            options.AddArgument("--disable-dev-shm-usage");
+            options.AddArgument("--disable-extensions");
+            options.AddArgument("--disable-features=TranslateUI,BlinkGenPropertyTrees");
+            options.AddArgument("--disable-ipc-flooding-protection");
+            options.AddArgument("--disable-renderer-backgrounding");
+            options.AddArgument("--enable-features=NetworkService,NetworkServiceInProcess");
+            options.AddArgument("--force-color-profile=srgb");
+            options.AddArgument("--hide-scrollbars");
+            options.AddArgument("--metrics-recording-only");
+            options.AddArgument("--mute-audio");
+            options.AddArgument("--headless");
+            options.AddArgument("--no-sandbox");
+
+            var driver = new RemoteWebDriver(new Uri("http://bl-01.westeurope.azurecontainer.io:3000/webdriver")
+                , options);
+
+            var baseUrl = "https://net.developerdays.pl/faq/";
+            driver.Navigate().GoToUrl(baseUrl);
+            driver.Manage().Window.Maximize();
+            await Task.Delay(1000);
+
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(driver.PageSource);
+
+            var nodes=doc.DocumentNode.SelectNodes("//div[contains(@class,'vc_toggle ')]").ToArray();
+            foreach (var node in nodes)
+            {
+                var title = node.SelectSingleNode("./div[@class='vc_toggle_title']").InnerText;
+                var text = node.SelectSingleNode("./div[@class='vc_toggle_content']").InnerText;
+                Console.WriteLine($"FAQ FOUND: ");
+                Console.WriteLine($"Question: {title}");
+                Console.WriteLine($"Answer: {text}");
+            }
+            
+
+        }
+
+        internal async Task SendEmail()
+        {
+            IWebDriver driver = new ChromeDriver(new ChromeOptions()
+            {
+
+            });
+
+            var baseUrl = "https://net.developerdays.pl/contact/";
+            driver.Navigate().GoToUrl(baseUrl);
+            await Task.Delay(1000);
+
+            driver.Manage().Window.Maximize();
+
+            driver.Fill("//input[@name='your-name']", "Roberto Freato");
+            driver.Fill("//input[@name='your-email']", "child@ocdstudio.net");
+            driver.Fill("//input[@name='your-subject']", "Hello NETDdays");
+            driver.Fill("//textarea[@name='your-message']", "Excuse me Maciej for the SPAM :)");
+            driver.Click("//input[@type='submit']");
+        }
+    }
+
+    public static class Extensions
+    {
+        public static IWebDriver Fill(this IWebDriver self, string xpath,string text)
+        {
+            var field = self.FindElement(By.XPath(xpath));
+            field.Click();
+            field.SendKeys(text);
+            return self;
+        }
+
+        public static IWebDriver Click(this IWebDriver self,string xpath)
+        {
+            self.FindElement(By.XPath(xpath)).Click();
+            return self;
+        }
+    }
+}
