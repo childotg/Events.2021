be1f63cb61a96372de7e4ffe2708337bda566187
childotg <child@ocdstudio.net>
2021-10-29 18:37:23 +0200

    AzConf updated

---
+++ 211029 - AzConf @India/JSONSQL/DataPrep/Database/MoviesHybrid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DataPrep.Database
+{
+    public partial class MoviesHybrid
+    {
+        public string MovieId { get; set; }
+        public string PlotSummary { get; set; }
+        public TimeSpan? Duration { get; set; }
+        public string Genre { get; set; }
+        public decimal? Rating { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public string PlotSynopsis { get; set; }
+        public string Reviews { get; set; }
+    }
+}
---
+++ 211029 - AzConf @India/JSONSQL/Database/json.fAsTimespan.sql
@@ -0,0 +1,13 @@
+﻿CREATE FUNCTION [json].[fAsTimespan](@textToClean AS NVARCHAR(MAX))
+RETURNS TIME
+AS
+BEGIN
+	RETURN TRY_CAST(TRIM(
+		REPLACE(
+			REPLACE(
+				REPLACE(@textToClean,' ',''),'h',':'),'min',':00')
+		)
+		AS TIME
+	);
+END
+GO
